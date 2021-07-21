f27b04ea52950102be33ce09ed7580062b539de9
GledfullyChill <stephan.lofstrom@gmail.com>
2021-07-21 17:00:46 -0600

    starting findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected ?
+    console.log(`🥰🥰 Assertion Passed: ${actual} === ${expected}`)
+    :
+    console.log(`😱Assertion Failed: ${actual} !== ${expected}`)
+  );
+};
+
+const findKeyByValue = function(obj, val) {
+  
+}
