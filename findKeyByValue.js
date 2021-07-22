18478ccd078d25f6ab219abe94d6b254097a8f6c
GledfullyChill <stephan.lofstrom@gmail.com>
2021-07-21 18:11:42 -0600

    filled out findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,5 +7,18 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKeyByValue = function(obj, val) {
-  
+  for (const item in obj) {
+    if (obj[item][val]) {
+      return item;
+    }
+  }
+  return;
 }
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
